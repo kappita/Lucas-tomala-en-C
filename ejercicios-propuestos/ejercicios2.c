56244a134347b1c03785b6584144c1afeb1a5938
Joaquín  Saldivia <97257980+Klefur@users.noreply.github.com>
2022-03-31 15:22:43 -0300

    se agrega dinamico sin terminar

--- ejercicios-propuestos/ejercicios2.c
+++ ejercicios-propuestos/ejercicios2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // variable generales
 int i;
@@ -54,8 +55,7 @@ void generar_matriz(int *A)
         for(j = 0; j < (filas_m); j++)
         {
             printf("Ingrese el valor %d %d de la matriz: ", i + 1, j + 1);
-            scanf("%d", A);
-            A++;
+            scanf("%d", &A[i][j]); 
         }        
     }
 }
@@ -99,21 +99,95 @@ int suma_matriz(int A[filas_m][filas_m])
     return total_m;
 }
 
+// ejercicio 9.6 dinamico
+int **crear_matriz_dinam(int **A, int n)
+{
+    A = malloc(sizeof(int *) * n);
+    
+    for(i = 0; i < n; i++)
+    {
+        A[i] = malloc(sizeof(int) * n);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            printf("Ingrese el valor %d %d de la matriz: ", i + 1, j + 1);
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    return A;
+}
+
+void imprimir_matriz_dim(int **A)
+{
+    printf("La matriz es: \n");
+
+    for(i = 0; i < filas_m; i++)
+    {
+        printf("(");
+        for(j = 0; j < filas_m; j++)
+        {
+            if( ((j+1) % filas_m) == 0)
+            {
+                printf("%d)\n", A[i][j]);
+            }
+            else
+            {
+                printf("%d ", A[i][j]);
+            }
+        }
+    }
+}
+
+int suma_matriz_dim(int **A)
+{
+    int total_m = 0;
+
+    for(i = 0; i < filas_m; i++)
+    {
+        for(j = 0; j < filas_m; j++)
+        {
+            if(j != i)
+            {
+                total_m += A[i][j];
+            }
+        }
+    }
+
+    return total_m;
+}
+
+
 int main()
 {
     // Ejercicio 9.5
     int matriz_1[filas_3][columnas_4];
 
-    leer_filas(matriz_1);
+    leer_filas(&matriz_1[0][0]);
 
     imprimir_columnas(matriz_1);
 
     // Ejercicio 9.6
     int matriz_2[filas_m][filas_m];
 
-    generar_matriz(matriz_2);
+    generar_matriz(&matriz_2[0][0]);
 
     imprimir_matriz(matriz_2);
 
     printf("El valor de la suma de la matriz sin la diagonal es: %d", suma_matriz(matriz_2));
+
+    // ejercicio 9.6 dinamico
+    int **matriz_dim, n;
+
+    printf("Ingrese el largo de la matriz: ");
+    scanf("%d", &n);
+
+    matriz_dim = crear_matriz_dinam(matriz_dim, n);
+
+    imprimir_matriz_dim(matriz_dim);
+
+    printf("El valor de la suma de la matriz sin la diagonal es: %d", suma_matriz_dim(matriz_dim));
 }
